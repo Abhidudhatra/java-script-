fa57e98930a2e29ad5f9a1f819d8c883b738839a
Abhi Dudhatra <dudhatraabhi@gmail.com>
2025-04-17 12:33:10 +0530

    datatypes and variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+ const accountId = 144553
+ let accountEmail = "abhidudhatra@gmail.com"
+ var accountPassword ="123456"
+ accountCity ="Rajkot"
+
+//  accountId = 123 not allowed 
+  
+//  console.log(accountId);
+
+accountEmail ="dudhatraabhi@yahoo.com"
+var accountPassword="456123"
+accountCity="Mumbai"
+let accountState
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+// prefer not to use var because in issues in block scope and fictional scope 
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+ "use strict";  // treat all js code as newer version 
+
+// alert(3+3)
+
+// console.log(3+3); 
+    
+// console.log("abhi dudhatra ")
+
+
+let name= "abhi"
+let age=18
+let isLoggedIn = false
+
+console.log(typeof(undefined)) //undefined
+console.log(typeof(null)); //object
+
+
+
+
