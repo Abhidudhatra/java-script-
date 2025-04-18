4d7350f0d7a298fbd4c4ecf588bf968a129b03e9
Abhi Dudhatra <dudhatraabhi@gmail.com>
2025-04-18 18:32:27 +0530

    commit done

--- 01_basics/Datatypes-summery.js
+++ 01_basics/Datatypes-summery.js
@@ -1,31 +1,59 @@
-// primitive 
-//string ,number, boolean,null undefined ,symbol,BigInt
+// // primitive 
+// //string ,number, boolean,null undefined ,symbol,BigInt
 
 
 
-const id=Symbol('123')
-const another_id=Symbol('123')
+// const id=Symbol('123')
+// const another_id=Symbol('123')
 
-console.log(id==another_id);
+// //console.log(id==another_id);
 
 
-const outsidetemp = null
-const isloggedin = false
-let useremail;
-const bignumber = 252525252525252n
+// const outsidetemp = null
+// const isloggedin = false
+// let useremail;
+// const bignumber = 252525252525252n
 
-// reference non primitive 
-// array,objects,function 
+// // reference non primitive 
+// // array,objects,function 
 
-const heros=["abhi","abhi","abhi"]
-let my_obj = {
-    name:"abhi",
-    age : 22,
-}
+// const heros=["abhi","abhi","abhi"]
+// let my_obj = {
+//     name:"abhi",
+//     age : 22,
+// }
 
-const myfunction =function(){
-    console.log("hello world");
+// // const myfunction =function(){
+// //     console.log("hello world");
     
-}
-console.log(typeof myfunction);
+// // }
+// // console.log(myfunction());
+// // //console.log(myfunction.call());
+
+
+// **************************************************************************************
+
+
+// stack (primitive )*********heap(non primitive )
+
+// let myname ="abhi"
+// let secondname = myname
+// secondname ="DUDHATRA"
+// console.log(secondname);
+
+// console.log(myname);
+
+// let userone={
+//     email:"xyz@yahoo.com",
+//     upi:"@ok.sbi"
+// }
+
+// let usertwo = userone
+// usertwo.email="xyz.gmail.com"
+// console.log(userone.email);
+// console.log(usertwo.email);
+
+
+
+
 
