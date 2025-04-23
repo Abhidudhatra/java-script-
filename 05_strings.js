05e7467067be16dba274f0627e0310b89a77f3e9
Abhi Dudhatra <dudhatraabhi@gmail.com>
2025-04-23 11:23:22 +0530

    strings

---
+++ 05_strings.js
@@ -0,0 +1,34 @@
+const gamename =new String('abhi-dudhatra')
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+
+const newString = gamename.substring(0,5)
+//console.log(newString);
+
+const anotherString =gamename.slice(6,8)
+//console.log(anotherString);
+
+const newString2 = "  abhi  ";
+//console.log(newString2);
+//console.log(newString2.trim());
+
+const url ='https://www.instagram.com/?hl=en';
+// console.log(url.replace('=','+'));
+// console.log(url.includes('instagram'));
+
+//console.log(gamename.split('-'));
+
+// string to array -> split 
+
+
+
+
+
+
+
+
+
+
